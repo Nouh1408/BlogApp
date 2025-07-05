33fdc14900587cb57f59dcb0b44d4670f492c5f3
Nouh1408 <ahmedinouh@gmail.com>
2025-07-05 20:21:37 +0300

    testing

--- index.js
+++ index.js
@@ -12,14 +12,38 @@ const dbconnection = mysql.createConnection({
 })
 dbconnection.connect((err)=>{
     if(err){
-        console.log("Failed to connect to DB");
+        console.log("Failed to connect to DB",err.message);
         
     } else{
         console.log("Logged to DB");
         
     }
 })
+//authentication --> Registeration,Login, forgetPassw
+app.use(express.json)//global
 
+app.post("/auth/register", (req,res,next)=>{
+    const {fName,lName,email,password,dob} = req.body;
+    console.log({fName,lName,email,password,dob});
+    let query = `SELECT * FROM users where email =?` //? avoids SQL injection
+    dbconnection.execute(query, [email],(err)=>{
+        if(err){
+            return res.status(500).json({message:"Server Error",success:false})
+        } 
+        if(result.length>0){
+            return res.status(409).json({message:"user already exist",success:true})
+        }
+        query = `INSERT INTO users (firstName,lastName,email,password,dob) VALUES (?,?,?,?,?)`
+        dbconnection.execute(query, [fName,lName,email,password,dob],(err,result)=>{
+            if(err){
+                return res.status(500).json({message:"Server Error", success:false})
+            }
+            return res.status(201).json({message:"user created Successfully",success:true, userID:result.insertId})
+
+        })
+    })
+    
+})
 
 
 
