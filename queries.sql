f78758c7e929d5ec1e6c43b7b8a1a9038065cd90
Nouh1408 <ahmedinouh@gmail.com>
2025-07-05 10:57:44 +0300

    finished with tables

--- queries.sql
+++ queries.sql
@@ -12,3 +12,29 @@ CREATE TABLE users(
     updatedAt timestamp DEFAULT current_timestamp on update current_timestamp,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE addresses(
+    id int not null AUTO_INCREMENT,
+    country varchar(20),
+    city varchar(20),
+    street varchar(20),
+    phone varchar(15),
+    userID int not null,
+    createdAt timestamp DEFAULT current_timestamp,
+    updatedAt timestamp DEFAULT current_timestamp on update current_timestamp,
+    PRIMARY KEY(id),
+    FOREIGN KEY (userID) REFERENCES users(id) ON DELETE CASCADE ON update CASCADE
+
+);
+
+CREATE TABLE blogs(
+    id int not null AUTO_INCREMENT,
+    userID int not null,
+    title varchar(20),
+    content text,
+    createdAt timestamp DEFAULT current_timestamp,
+    updatedAt timestamp DEFAULT current_timestamp on update current_timestamp,
+    PRIMARY KEY(id),
+    FOREIGN KEY (userID) REFERENCES users(id) ON DELETE CASCADE ON update CASCADE
+
+);
